a97d121e9252e800448a944c11670395b83a2424
adam-back <adamback42@gmail.com>
2014-09-19 15:57:19 -0700

    Replaces circles with asteroids

--- watchout.js
+++ watchout.js
@@ -1,28 +1,40 @@
 // start slingin' some d3 here.
+var update = function(data) {
+  d3.select("svg").select("image.asteroid")
+    .data(data)
+    .enter()
+    .append("image")
+    .style("height", function() {
+      return "50";
+    })
+    .style("width", function() {
+      return "50";
+    })
+    .attr("class", "asteroid")
+    .style("x", function(d) {
+      return d[0];
+    })
+    .style("y", function(d) {
+      return d[1];
+    })
+}
 
-d3.select("svg").select("circle.enemy")
-  .data([cx,cy])
-  .enter()
-  .append("circle")
-  .style("r", function() {
-    return "25";
-  })
-  .style("fill", function() {
-    return "red";
-  })
-  .attr("class", "enemy")
-  .style("cx", function(d) {
-    return d[0];
-  })
-  .style("cy", function(d) {
-    return d[1];
-  })
-
-var randomEnemyLocation = function() {
+var asteroidLocation = function() { //generates one enemy location
   var tuple = []
   tuple[0] = Math.floor(Math.random() * 700);
   tuple[1] = Math.floor(Math.random() * 700);
   return tuple;
 }
 
+var asteroidField = function(n) { //generates locations for each enemy in fleet
+  var counter = 0;
+  var locData = [];
+  while (counter <= n) {
+    locData.push(asteroidLocation());
+    counter++
+  }
+  return locData;
+}
+
+update(asteroidField(10));
 
