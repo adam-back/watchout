b0b2b7b1bd230fee5587de6137b3f6cee72446df
adam-back <adamback42@gmail.com>
2014-09-19 16:39:21 -0700

    Shows all asteroids

--- watchout.js
+++ watchout.js
@@ -1,40 +1,50 @@
 // start slingin' some d3 here.
 var update = function(data) {
-  d3.select("svg").select("image.asteroid")
+  d3.select("body").select("svg").selectAll("image.asteroid")
     .data(data)
     .enter()
     .append("image")
-    .style("height", function() {
+    .attr("height", function() {
       return "50";
     })
-    .style("width", function() {
+    .attr("width", function() {
       return "50";
     })
     .attr("class", "asteroid")
-    .style("x", function(d) {
+    .attr("x", function(d) {
       return d[0];
     })
-    .style("y", function(d) {
+    .attr("y", function(d) {
       return d[1];
     })
-}
+    .attr("xlink:href", function() {
+      return "asteroid.png";
+    })
+};
+
+// debugger;
+
+// console.log(typeof asteroidField === "function");
+// console.log(asteroidField);
+// console.log(typeof update === "function");
+
 
 var asteroidLocation = function() { //generates one enemy location
   var tuple = []
   tuple[0] = Math.floor(Math.random() * 700);
   tuple[1] = Math.floor(Math.random() * 700);
   return tuple;
-}
+};
 
 var asteroidField = function(n) { //generates locations for each enemy in fleet
   var counter = 0;
   var locData = [];
   while (counter <= n) {
     locData.push(asteroidLocation());
-    counter++
+    counter++;
   }
   return locData;
-}
+};
 
 update(asteroidField(10));
 
