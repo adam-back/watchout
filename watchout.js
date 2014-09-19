6a606f2caf704989dfc1429bf986f99985ea5c54
adam-back <adamback42@gmail.com>
2014-09-19 15:40:25 -0700

    Added d3 update function. Functions which create asteroid location data.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,28 @@
 // start slingin' some d3 here.
 
+d3.select("svg").select("circle.enemy")
+  .data([cx,cy])
+  .enter()
+  .append("circle")
+  .style("r", function() {
+    return "25";
+  })
+  .style("fill", function() {
+    return "red";
+  })
+  .attr("class", "enemy")
+  .style("cx", function(d) {
+    return d[0];
+  })
+  .style("cy", function(d) {
+    return d[1];
+  })
+
+var randomEnemyLocation = function() {
+  var tuple = []
+  tuple[0] = Math.floor(Math.random() * 700);
+  tuple[1] = Math.floor(Math.random() * 700);
+  return tuple;
+}
+
+
